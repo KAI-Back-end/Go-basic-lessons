c053c331b61f9f120f18dacbf590ead0a7b5d142
ivanovamir <adamstradvers@gmail.com>
2024-04-13 23:25:55 +0300

    [LEC4] add task

---
+++ lection_4/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+const maxPoolConn = 3
+
+type Server struct{}
+
+// Функ-ия, которая обрабатывает запрос пользователя.
+func (s *Server) ServeUsers(userID int64) string {
+	// какая-то полезная нагрузка
+	return fmt.Sprintf("User %d is served", userID)
+}
+
+// Функ-ия, ответственная за обработку поступающих запросов пользователей приложения.
+func Serve(req <-chan int64) {}
