4a37755f867dcc2599a0a1946ea071a41e69adff
Pasca11 <dr.marshall.work@gmail.com>
2024-04-14 19:21:44 +0300

    task solved

--- lection_4/main.go
+++ lection_4/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync/atomic"
+	"time"
 )
 
 const maxPoolConn = 3
@@ -14,5 +16,30 @@ func (s *Server) ServeUser(userID int64) string {
 	return fmt.Sprintf("User %d is served", userID)
 }
 
+func main() {
+	req := make(chan int64)
+	go Serve(req)
+
+	for i := 0; i < 5; i++ {
+		req <- int64(i)
+	}
+
+	time.Sleep(time.Second * 10)
+}
+
 // Функ-ия, ответственная за обработку поступающих запросов пользователей приложения.
-func Serve(req <-chan int64) {}
+func Serve(req <-chan int64) {
+	s := &Server{}
+	var counter atomic.Int32
+	for v := range req {
+		//go func(val int64) {
+		for counter.Load() >= maxPoolConn {
+			time.Sleep(time.Second)
+		}
+		go func(val int64) {
+			counter.Add(1)
+			fmt.Println(s.ServeUser(val))
+			counter.Add(-1)
+		}(v)
+	}
+}
