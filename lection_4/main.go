9cbe8b18caae11ea7d4699a1ba76c24120cb7994
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-04-18 15:15:19 +0300

    Update main.go

--- lection_4/main.go
+++ lection_4/main.go
@@ -15,4 +15,36 @@ func (s *Server) ServeUser(userID int64) string {
 }
 
 // Функ-ия, ответственная за обработку поступающих запросов пользователей приложения.
-func Serve(req <-chan int64) {}
+func Serve(req <-chan int64) {
+	server := &Server{}
+	ch := make(chan int64, maxPoolConn)
+	count := 0
+
+	for cur := range req {
+		ch <- int64(cur)
+
+		if count >= maxPoolConn {
+			time.Sleep(5 * time.Second)
+		}
+
+		go func() {
+			count = count + 1
+			fmt.Println(server.ServeUser(<-ch))
+			fmt.Println(count)
+			time.Sleep(1 * time.Second)
+			count = count - 1
+		}()
+	}
+}
+
+func main() {
+	req := make(chan int64)
+	go Serve(req)
+
+	for i := 0; i < 5; i++ {
+		req <- int64(i)
+	}
+
+	time.Sleep(1 * time.Second)
+}
+
