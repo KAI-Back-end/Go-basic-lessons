97ede5666efda0410256d38de700d051cd2880a4
Pasca11 <dr.marshall.work@gmail.com>
2024-05-03 17:22:02 +0300

    task 2 solved

--- lection_5/2/main.go
+++ lection_5/2/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
 func main() {
 	var urls = []string{
 		"https://ya.ru",
@@ -11,4 +18,36 @@ func main() {
 		"https://test.ru",
 		"https://example.com/",
 	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	ctx, cancel := context.WithCancel(context.Background())
+	for _, url := range urls {
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			go func(url_ string, wg_ *sync.WaitGroup) {
+				resp, err := http.Get(url_)
+				select {
+				case <-ctx.Done():
+					return
+				default:
+
+				}
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				status := resp.StatusCode
+				fmt.Println(url_, status)
+				if status == 200 {
+					wg_.Done()
+				}
+			}(url, wg)
+		}
+	}
+	wg.Wait()
+	cancel()
+	fmt.Println("Done")
 }
