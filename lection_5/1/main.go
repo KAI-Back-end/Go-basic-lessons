e8795c0f7963da7367f4a936fc339d1de1ec996a
Pasca11 <dr.marshall.work@gmail.com>
2024-05-03 13:06:38 +0300

    task 1 solved

--- lection_5/1/main.go
+++ lection_5/1/main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	ch := make(chan struct{})
 
-	ch <- struct{}{} // Deadlock
+	select {
+	case ch <- struct{}{}:
+		fmt.Println("Success")
+	default:
+		fmt.Println("Cant send message")
+	}
 }
